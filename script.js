373077e95962ad37e9fb5a690e9ae3557cbc647d
Célian <lherbier@MacBook-Air-de-Celian.local>
2026-01-07 17:25:58 +0100

    fonction boucle question

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
+let currentQuestion = 0;
+
 function startQuiz() {
+    const startSection = document.getElementById("StarQuiz");
+    startSection.classList.add("hidden");
+
+    currentQuestion = 1;
+
+    let questionElement = document.getElementById("Question" + currentQuestion);
+
+    questionElement.classList.remove("hidden");
     console.log("Quiz started");
 }
 
+function NextQuestion() {
+    currentQuestion++;
+    let nextElement = document.getElementById("Question" + currentQuestion);
+}
